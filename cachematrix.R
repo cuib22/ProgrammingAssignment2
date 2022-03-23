d45291cb64c2cd92c28bc50400f3ebe633406212
cuib22 <b.cui22@gmail.com>
2022-03-23 15:33:20 +0100

    Commiting Version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix() generate a list. x is supposed to be a invertible matrix.
+### mset() is used to update new squred matrix.
+### to run this function, attribute makeCacheMatrix(x) to an object.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv<-NULL
+        if (!nrow(x)==ncol(x)|det(x) == 0){
+                print("This matrix is not invertible")
+                x<<-NULL
+        } 
+        mset<-function(y=matrix()){
+                if (!nrow(y)==ncol(y)|det(y) == 0){
+                        print("This matrix is not invertible")
+                        x1<<-NULL
+                } else {
+                        x1<<-y
+                }
+                x<<-x1
+        }
+        m<-x
+        mget<-function()m
+        invset<-function(inverse) {inv<<-inverse}
+        invget<-function()inv
+        list(mset = mset, mget = mget, invset = invset, invget=invget)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve is used to cache the inverse matrix given by makeCacheMatrix().
 
 cacheSolve <- function(x, ...) {
+        inv<-x$invget()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        } 
         ## Return a matrix that is the inverse of 'x'
+        inv<-solve(x$mget())
+        x$invset(inv)
+        inv
 }
